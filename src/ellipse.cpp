ea114cb7fc048da8b60c52b534fbe05c4257d5aa
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2018-03-11 21:09:50 +0100

    [src] Correction of small bugs + add publisher only on activated log

--- src/ellipse.cpp
+++ src/ellipse.cpp
@@ -37,25 +37,44 @@ void Ellipse::init_callback(){
 }
 
 void Ellipse::init_publishers(){
-  m_sbgStatus_pub = m_node->advertise<sbg_driver::SbgStatus>("status",10);
-  m_sbgUtcTime_pub = m_node->advertise<sbg_driver::SbgUtcTime>("utc_time",10);
-  m_sbgImuData_pub = m_node->advertise<sbg_driver::SbgImuData>("imu_data",10);
-  m_sbgEkfEuler_pub = m_node->advertise<sbg_driver::SbgEkfEuler>("ekf_euler",10);
-  m_sbgEkfQuat_pub = m_node->advertise<sbg_driver::SbgEkfQuat>("ekf_quat",10);
-  m_sbgEkfNav_pub = m_node->advertise<sbg_driver::SbgEkfNav>("ekf_nav",10);
-  m_sbgShipMotion_pub = m_node->advertise<sbg_driver::SbgShipMotion>("ship_motion",10);
-  m_sbgMag_pub = m_node->advertise<sbg_driver::SbgMag>("mag",10);
-  m_sbgMagCalib_pub = m_node->advertise<sbg_driver::SbgMagCalib>("mag_calib",10);
-  m_sbgGpsVel_pub = m_node->advertise<sbg_driver::SbgGpsVel>("gps_vel",10);
-  m_sbgGpsPos_pub = m_node->advertise<sbg_driver::SbgGpsPos>("gps_pos",10);
-  m_sbgGpsHdt_pub = m_node->advertise<sbg_driver::SbgGpsHdt>("gps_hdt",10);
-  m_sbgGpsRaw_pub = m_node->advertise<sbg_driver::SbgGpsRaw>("gps_raw",10);
-  m_sbgOdoVel_pub = m_node->advertise<sbg_driver::SbgOdoVel>("odo_vel",10);
-  m_sbgEventA_pub = m_node->advertise<sbg_driver::SbgEvent>("eventA",10);
-  m_sbgEventB_pub = m_node->advertise<sbg_driver::SbgEvent>("eventB",10);
-  m_sbgEventC_pub = m_node->advertise<sbg_driver::SbgEvent>("eventC",10);
-  m_sbgEventD_pub = m_node->advertise<sbg_driver::SbgEvent>("eventD",10);
-  m_sbgPressure_pub = m_node->advertise<sbg_driver::SbgPressure>("pressure",10);
+  if(m_log_status!=0)
+    m_sbgStatus_pub = m_node->advertise<sbg_driver::SbgStatus>("status",10);
+  if(m_log_utc_time !=0)
+    m_sbgUtcTime_pub = m_node->advertise<sbg_driver::SbgUtcTime>("utc_time",10);
+  if(m_log_imu_data !=0)
+    m_sbgImuData_pub = m_node->advertise<sbg_driver::SbgImuData>("imu_data",10);
+  if(m_log_ekf_euler !=0)
+    m_sbgEkfEuler_pub = m_node->advertise<sbg_driver::SbgEkfEuler>("ekf_euler",10);
+  if(m_log_ekf_quat !=0)
+    m_sbgEkfQuat_pub = m_node->advertise<sbg_driver::SbgEkfQuat>("ekf_quat",10);
+  if(m_log_ekf_nav !=0)
+    m_sbgEkfNav_pub = m_node->advertise<sbg_driver::SbgEkfNav>("ekf_nav",10);
+  if(m_log_ship_motion !=0)
+    m_sbgShipMotion_pub = m_node->advertise<sbg_driver::SbgShipMotion>("ship_motion",10);
+  if(m_log_mag !=0)
+    m_sbgMag_pub = m_node->advertise<sbg_driver::SbgMag>("mag",10);
+  if(m_log_mag_calib !=0)
+    m_sbgMagCalib_pub = m_node->advertise<sbg_driver::SbgMagCalib>("mag_calib",10);
+  if(m_log_gps1_vel !=0)
+    m_sbgGpsVel_pub = m_node->advertise<sbg_driver::SbgGpsVel>("gps_vel",10);
+  if(m_log_gps1_pos !=0)
+    m_sbgGpsPos_pub = m_node->advertise<sbg_driver::SbgGpsPos>("gps_pos",10);
+  if(m_log_gps1_hdt !=0)
+    m_sbgGpsHdt_pub = m_node->advertise<sbg_driver::SbgGpsHdt>("gps_hdt",10);
+  if(m_log_gps1_raw !=0)
+    m_sbgGpsRaw_pub = m_node->advertise<sbg_driver::SbgGpsRaw>("gps_raw",10);
+  if(m_log_odo_vel !=0)
+    m_sbgOdoVel_pub = m_node->advertise<sbg_driver::SbgOdoVel>("odo_vel",10);
+  if(m_log_event_a !=0)
+    m_sbgEventA_pub = m_node->advertise<sbg_driver::SbgEvent>("eventA",10);
+  if(m_log_event_b !=0)
+    m_sbgEventB_pub = m_node->advertise<sbg_driver::SbgEvent>("eventB",10);
+  if(m_log_event_c !=0)
+    m_sbgEventC_pub = m_node->advertise<sbg_driver::SbgEvent>("eventC",10);
+  if(m_log_event_d !=0)
+    m_sbgEventD_pub = m_node->advertise<sbg_driver::SbgEvent>("eventD",10);
+  if(m_log_pressure !=0)
+    m_sbgPressure_pub = m_node->advertise<sbg_driver::SbgPressure>("pressure",10);
 }
 
 void Ellipse::configure(){
@@ -201,97 +220,97 @@ void Ellipse::publish(){
   SbgErrorCode errorCode = sbgEComHandle(&m_comHandle);
   if (errorCode != SBG_NO_ERROR){ROS_WARN("SBG DRIVER - sbgEComHandle Error : %s", sbgErrorCodeToString(errorCode));}
 
-  if(m_new_sbgStatus){
+  if(m_new_sbgStatus && m_log_status != 0){
     m_new_sbgStatus = false;
     m_sbgStatus_pub.publish(m_sbgStatus_msg);
   }
 
-  if(m_new_sbgUtcTime){
+  if(m_new_sbgUtcTime && m_log_utc_time != 0){
     m_new_sbgUtcTime = false;
     m_sbgUtcTime_pub.publish(m_sbgUtcTime_msg);
   }
 
-  if(m_new_sbgImuData){
+  if(m_new_sbgImuData && m_log_imu_data != 0){
     m_new_sbgImuData = false;
     m_sbgImuData_pub.publish(m_sbgImuData_msg);
   }
 
-  if(m_new_sbgEkfEuler){
+  if(m_new_sbgEkfEuler && m_log_ekf_euler != 0){
     m_new_sbgEkfEuler = false;
     m_sbgEkfEuler_pub.publish(m_sbgEkfEuler_msg);
   }
 
-  if(m_new_sbgEkfQuat){
+  if(m_new_sbgEkfQuat && m_log_ekf_quat != 0){
     m_new_sbgEkfQuat = false;
     m_sbgEkfQuat_pub.publish(m_sbgEkfQuat_msg);
   }
 
-  if(m_new_sbgEkfNav){
+  if(m_new_sbgEkfNav && m_log_ekf_nav != 0){
     m_new_sbgEkfNav = false;
     m_sbgEkfNav_pub.publish(m_sbgEkfNav_msg);
   }
 
-  if(m_new_sbgShipMotion){
+  if(m_new_sbgShipMotion && m_log_ship_motion != 0){
     m_new_sbgShipMotion = false;
     m_sbgShipMotion_pub.publish(m_sbgShipMotion_msg);
   }
 
-  if(m_new_sbgMag){
+  if(m_new_sbgMag && m_log_mag != 0){
     m_new_sbgMag = false;
     m_sbgMag_pub.publish(m_sbgMag_msg);
   }
 
-  if(m_new_sbgMagCalib){
+  if(m_new_sbgMagCalib && m_log_mag_calib != 0){
     m_new_sbgMagCalib = false;
     m_sbgMagCalib_pub.publish(m_sbgMagCalib_msg);
   }
 
-  if(m_new_sbgGpsVel){
+  if(m_new_sbgGpsVel && m_log_gps1_vel != 0){
     m_new_sbgGpsVel = false;
     m_sbgGpsVel_pub.publish(m_sbgGpsVel_msg);
   }
 
-  if(m_new_sbgGpsPos){
+  if(m_new_sbgGpsPos && m_log_gps1_pos != 0){
     m_new_sbgGpsPos = false;
     m_sbgGpsPos_pub.publish(m_sbgGpsPos_msg);
   }
 
-  if(m_new_sbgGpsHdt){
+  if(m_new_sbgGpsHdt && m_log_gps1_hdt != 0){
     m_new_sbgGpsHdt = false;
     m_sbgGpsHdt_pub.publish(m_sbgGpsHdt_msg);
   }
 
-  if(m_new_sbgGpsRaw){
+  if(m_new_sbgGpsRaw && m_log_gps1_raw != 0){
     m_new_sbgGpsRaw = false;
     m_sbgGpsRaw_pub.publish(m_sbgGpsRaw_msg);
   }
 
-  if(m_new_sbgOdoVel){
+  if(m_new_sbgOdoVel && m_log_odo_vel != 0){
     m_new_sbgOdoVel = false;
     m_sbgOdoVel_pub.publish(m_sbgOdoVel_msg);
   }
 
-  if(m_new_sbgEventA){
+  if(m_new_sbgEventA && m_log_event_a != 0){
     m_new_sbgEventA = false;
     m_sbgEventA_pub.publish(m_sbgEventA_msg);
   }
 
-  if(m_new_sbgEventB){
+  if(m_new_sbgEventB && m_log_event_b != 0){
     m_new_sbgEventB = false;
     m_sbgEventB_pub.publish(m_sbgEventB_msg);
   }
 
-  if(m_new_sbgEventC){
+  if(m_new_sbgEventC && m_log_event_c != 0){
     m_new_sbgEventC = false;
     m_sbgEventC_pub.publish(m_sbgEventC_msg);
   }
 
-  if(m_new_sbgEventD){
+  if(m_new_sbgEventD && m_log_event_d != 0){
     m_new_sbgEventD = false;
     m_sbgEventD_pub.publish(m_sbgEventD_msg);
   }
 
-  if(m_new_sbgPressure){
+  if(m_new_sbgPressure && m_log_pressure != 0){
     m_new_sbgPressure = false;
     m_sbgPressure_pub.publish(m_sbgPressure_msg);
   }
@@ -605,126 +624,126 @@ bool Ellipse::set_cmd_output(){
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_UTC_TIME,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_utc_time){
     pConf = (SbgEComOutputMode) m_log_utc_time;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_UTC_TIME, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_IMU_DATA,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_imu_data){
     pConf = (SbgEComOutputMode) m_log_imu_data;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_IMU_DATA, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_MAG,&pConf);
-  if(pConf != m_log_status){
-    pConf = (SbgEComOutputMode) m_log_mag;
+  if(pConf != m_log_ekf_euler){
+    pConf = (SbgEComOutputMode) m_log_ekf_euler;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_MAG, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_MAG_CALIB,&pConf);
-  if(pConf != m_log_status){
-    pConf = (SbgEComOutputMode) m_log_mag_calib;
+  if(pConf != m_log_ekf_quat){
+    pConf = (SbgEComOutputMode) m_log_ekf_quat;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_MAG_CALIB, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EKF_EULER,&pConf);
-  if(pConf != m_log_status){
-    pConf = (SbgEComOutputMode) m_log_ekf_euler;
+  if(pConf != m_log_ekf_nav){
+    pConf = (SbgEComOutputMode) m_log_ekf_nav;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EKF_EULER, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EKF_QUAT,&pConf);
-  if(pConf != m_log_status){
-    pConf = (SbgEComOutputMode) m_log_ekf_quat;
+  if(pConf != m_log_ship_motion){
+    pConf = (SbgEComOutputMode) m_log_ship_motion;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EKF_QUAT, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EKF_NAV,&pConf);
-  if(pConf != m_log_status){
-    pConf = (SbgEComOutputMode) m_log_ekf_nav;
+  if(pConf != m_log_mag){
+    pConf = (SbgEComOutputMode) m_log_mag;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EKF_NAV, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_SHIP_MOTION,&pConf);
-  if(pConf != m_log_status){
-    pConf = (SbgEComOutputMode) m_log_ship_motion;
+  if(pConf != m_log_mag_calib){
+    pConf = (SbgEComOutputMode) m_log_mag_calib;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_SHIP_MOTION, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_GPS1_VEL,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_gps1_vel){
     pConf = (SbgEComOutputMode) m_log_gps1_vel;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_GPS1_VEL, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_GPS1_POS,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_gps1_pos){
     pConf = (SbgEComOutputMode) m_log_gps1_pos;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_GPS1_POS, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_GPS1_HDT,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_gps1_hdt){
     pConf = (SbgEComOutputMode) m_log_gps1_hdt;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_GPS1_HDT, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_GPS1_RAW,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_gps1_raw){
     pConf = (SbgEComOutputMode) m_log_gps1_raw;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_GPS1_RAW, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_ODO_VEL,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_odo_vel){
     pConf = (SbgEComOutputMode) m_log_odo_vel;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_ODO_VEL, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EVENT_A,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_event_a){
     pConf = (SbgEComOutputMode) m_log_event_a;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EVENT_A, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EVENT_B,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_event_b){
     pConf = (SbgEComOutputMode) m_log_event_b;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EVENT_B, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EVENT_C,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_event_c){
     pConf = (SbgEComOutputMode) m_log_event_c;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EVENT_C, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EVENT_D,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_event_d){
     pConf = (SbgEComOutputMode) m_log_event_d;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_EVENT_D, pConf);
     change = true;
   }
 
   sbgEComCmdOutputGetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_PRESSURE,&pConf);
-  if(pConf != m_log_status){
+  if(pConf != m_log_pressure){
     pConf = (SbgEComOutputMode) m_log_pressure;
     sbgEComCmdOutputSetConf(&m_comHandle, m_portOutput, SBG_ECOM_CLASS_LOG_ECOM_0, SBG_ECOM_LOG_PRESSURE, pConf);
     change = true;
